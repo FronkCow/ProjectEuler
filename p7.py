78aadfc6afb9a85bc72050326862faef297b8388
Frank Cao <frankyjcao@gmail.com>
2017-11-12 22:01:27 -0500

    initial attempt at p7, still wip

--- p7.py
+++ p7.py
@@ -1,5 +1,20 @@
 # What is the 10 001st prime number?
 import sys
+import math
 
-max_int = sys.maxsize
-print(max_int)
+# maybe too big lol
+# max_num = sys.maxsize
+# print(max_num)
+
+arb_limit = 200000000
+
+list_all = [True] * arb_limit
+list_prime = []
+
+for x in range(2, int(math.sqrt(len(list_all)))):
+    if list_all[x] == True:
+        for y in range(x * x, len(list_all), x):
+            list_all[y] = False
+            list_prime.append(y)
+
+print(list_prime)
